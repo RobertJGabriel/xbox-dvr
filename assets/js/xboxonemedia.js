646fa8f5f233fdbb482ff53b35261d29d5c17c82
Robert Gabriel <robert_gabriel@outlook.com>
2016-01-02 01:28:56 +0000

    clear code

--- assets/js/xboxonemedia.js
+++ assets/js/xboxonemedia.js
@@ -127,18 +127,12 @@ function renderMedia(flag, limits) {
     var randomNumber = Math.floor(Math.random() * mediaSrc.length) + 0;
     if (flag === 1) {
         for (i = 0; i < mediaSrc.length; i++) {
-            if (getType === 'gameclips') {
-                createVideo(mediaSrc[i], screenPreview[i], title[i], uploadTime[i]);
-            } else {
-                createImage(mediaSrc[i], screenPreview[i], title[i], uploadTime[i]);
-            }
+            getType === 'gameclips' ? createVideo(mediaSrc[i], screenPreview[i], title[i], uploadTime[i]) : createImage(mediaSrc[i], screenPreview[i], title[i], uploadTime[i]);
+            
         }
     } else {
-        if (getType === 'gameclips') {
-            createVideo(mediaSrc[randomNumber], screenPreview[randomNumber], title[randomNumber], uploadTime[randomNumber]);
-        } else {
-            createImage(mediaSrc[randomNumber], screenPreview[randomNumber], title[randomNumber], uploadTime[randomNumber]);
-        }
+    getType === 'gameclips' ? createVideo(mediaSrc[randomNumber], screenPreview[randomNumber], title[randomNumber], uploadTime[randomNumber]) :
+            createImage(mediaSrc[randomNumber], screenPreview[randomNumber], title[randomNumber], uploadTime[randomNumber]);   
     }
     myBtn.innerHTML = "Load More"; //Clear div
 }
